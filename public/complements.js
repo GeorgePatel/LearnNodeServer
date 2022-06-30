80a8d867a852c42a722822f94ded030884297837
GeorgePatel <gpatel1126@gmail.com>
2022-06-29 20:48:51 -0400

    Clicking the Insult button now has a Pitbull bullying the user. Styled the page nicely using CSS. Could not get Parcel and PopMotion working on the button.

--- public/complements.js
+++ public/complements.js
@@ -13,12 +13,29 @@ document
             });
     });
 
+document
+    .querySelector(".request-insult")
+    .addEventListener("click", function() {
+        fetch("/insult")
+            .then(function(res) {
+                return res.json();
+            })
+            .then(function(data) {
+                document.querySelector(".insult").innerText = data.insult;
+            })
+            .catch(function(err) {
+                console.error(err);
+            });
+    });
+
 const shiba = document.querySelector(".shiba");
+const pitbull = document.querySelector(".pitbull");
 const shiba_url = "https://dog.ceo/api/breed/shiba/images/random";
+const pitbull_url = "https://dog.ceo/api/breed/pitbull/images/random";
 const pic_length = 300;
 
 function addNewShiba() {
-    
+
     if (shiba.hasChildNodes()) {
         shiba.removeChild(shiba.childNodes.item(0));
         // because removing the first element moves the second element to the first index
@@ -35,8 +52,10 @@ function addNewShiba() {
             return processingPromise;
         })
         .then(function(processedResponse) {
-            const shiba_pic = createShibaImgTag(processedResponse.message);
+            const shiba_pic = createImgTag(processedResponse.message);
             const shiba_msg = createShibaMsg();
+            shiba_pic.alt = "Cute shiba";
+            shiba_pic.className = "shiba";
 
             shiba.removeChild(loadingGif);
             shiba.appendChild(shiba_pic);
@@ -44,23 +63,50 @@ function addNewShiba() {
         });
 }
 
-function createShibaImgTag(img_url) {
+function addNewPitbull() {
 
-    const shiba_pic = document.createElement("img");
-    shiba_pic.src = img_url;
+    if (pitbull.hasChildNodes()) {
+        pitbull.removeChild(pitbull.childNodes.item(0));
+        // because removing the first element moves the second element to the first index
+        pitbull.removeChild(pitbull.childNodes.item(0));
+    }
 
-    shiba_pic.width = pic_length;
-    shiba_pic.height = pic_length;
-    shiba_pic.alt = "Cute shiba";
-    shiba_pic.className = "shiba";
-    return shiba_pic;
+    const loadingGif = createLoadingGif();
+    pitbull.appendChild(loadingGif);
+
+    const promise = fetch(pitbull_url);
+    promise
+        .then(function(response) {
+            const processingPromise = response.json();
+            return processingPromise;
+        })
+        .then(function(processedResponse) {
+            const pitbull_pic = createImgTag(processedResponse.message);
+            const pitbull_msg = createPitbullLMsg();
+            pitbull_pic.alt = "Mean pitbull";
+            pitbull_pic.className = "pitbull";
+
+            pitbull.removeChild(loadingGif);
+            pitbull.appendChild(pitbull_pic);
+            pitbull.appendChild(pitbull_msg);
+        });
+}
+
+function createImgTag(img_url) {
+
+    const dog_pic = document.createElement("img");
+    dog_pic.src = img_url;
+    dog_pic.width = pic_length;
+    dog_pic.height = pic_length;
+
+    return dog_pic;
 
 }
 
 function createLoadingGif() {
 
     const img_tag = document.createElement("img");
-    img_tag.src = "https://media3.giphy.com/media/3o7bu3XilJ5BOiSGic/giphy.gif";
+    img_tag.src = "https://i.gifer.com/VAyR.gif";
 
     img_tag.width = pic_length;
     img_tag.height = pic_length;
@@ -75,5 +121,15 @@ function createShibaMsg() {
     return msg_tag;
 }
 
+function createPitbullLMsg() {
+    const msg_tag = document.createElement("p");
+    msg_tag.innerText = "Pitbull tells you to calm down!";
+
+    return msg_tag;
+}
+
 document.querySelector(".request-complement")
     .addEventListener("click", addNewShiba);
+
+document.querySelector(".request-insult")
+    .addEventListener("click", addNewPitbull);
