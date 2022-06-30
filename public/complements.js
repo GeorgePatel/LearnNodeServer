efb8b9e8a418b9f5d98d0b5ce5eb694ce9fad359
GeorgePatel <gpatel1126@gmail.com>
2022-06-30 07:04:00 -0400

    Popmotion working on the Complement and Insult buttons

--- public/complements.js
+++ public/complements.js
@@ -133,3 +133,32 @@ document.querySelector(".request-complement")
 
 document.querySelector(".request-insult")
     .addEventListener("click", addNewPitbull);
+
+
+import { styler, value, listen, pointer, spring } from "popmotion";
+
+const complementBall = document.querySelector(".request-complement");
+const insultBall = document.querySelector(".request-insult");
+
+makeItBounce(complementBall);
+makeItBounce(insultBall);
+
+function makeItBounce(ball) {
+
+    const divStyler = styler(ball);
+    const ballXY = value({ x: 0, y: 0 }, divStyler.set);
+
+    listen(ball, "mousedown touchstart").start(e => {
+        e.preventDefault();
+        pointer(ballXY.get()).start(ballXY);
+    });
+
+    listen(document, "mouseup touchend").start(() => {
+        spring({
+            from: ballXY.get(),
+            velocity: ballXY.getVelocity(),
+            to: { x: 0, y: 0 },
+            stiffness: 200
+        }).start(ballXY);
+    });
+}
